f2025c25639fedd236569b7449b9192586b35ef4
Andzej Korovacki <Andrzej.korovacki@gmail.com>
2023-05-14 22:38:31 +0300

    [Off-Story] Test dependencies removed (#1)

--- internal/lithuanian/converter_test.go
+++ internal/lithuanian/converter_test.go
@@ -3,8 +3,8 @@ package lithuanian_test
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/unknovvn/numtoword/internal/lithuanian"
+	"github.com/unknovvn/numtoword/test/assert"
 )
 
 func TestNumToWord_CorrectlyConvertsPlusMinusNineteen(t *testing.T) {
